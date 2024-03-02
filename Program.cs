2c569042a59a364232fed898c6c8bf86dae4393d
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-03-03 00:10:30 +0500

    Additional continuation

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 ﻿double num1, num2, result;
 char operation;
+string DavomEt;
+Salom:
 System.Console.WriteLine("Kalkulyatorga xush kelibsiz!");
-System.Console.WriteLine("Istalgan operatorni tanlang (+, -, /, *):");
+System.Console.Write("Birinchi sonni kiriting: ");
+num1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Operator kiriting: ");
 operation = Convert.ToChar(Console.ReadLine());
 
-System.Console.WriteLine("Ikkita sonni kiriting:");
-num1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Ikkinchi sonni kiriting: ");
 num2 = Convert.ToDouble(Console.ReadLine());
 
 switch (operation)
@@ -13,11 +16,33 @@ switch (operation)
     case '+':
     result = num1 + num2;
     System.Console.WriteLine("Natija: " + result);
+    System.Console.WriteLine("Davom etasizmi!");
+        DavomEt = Console.ReadLine();
+        if(DavomEt == "yes")
+        {
+            goto Salom;
+        }
+        else if(DavomEt == "NO")
+        {
+            System.Console.WriteLine("Goodbay");
+            break;
+        }
     break;
 
     case '-':
     result = num1 - num2;
     System.Console.WriteLine("Natija: " + result);
+    System.Console.WriteLine("Davom etasizmi!");
+        DavomEt = Console.ReadLine();
+        if(DavomEt == "yes")
+        {
+            goto Salom;
+        }
+        else if(DavomEt == "NO")
+        {
+            System.Console.WriteLine("Goodbay");
+            break;
+        }
     break;
 
     case '/':
@@ -30,11 +55,33 @@ switch (operation)
     {
         System.Console.WriteLine("Nolga bo'lish mumkin emas!");
     }
+    System.Console.WriteLine("Davom etasizmi!");
+        DavomEt = Console.ReadLine();
+        if(DavomEt == "yes")
+        {
+            goto Salom;
+        }
+        else if(DavomEt == "NO")
+        {
+            System.Console.WriteLine("Goodbay");
+            break;
+        }
     break;
 
     case '*':
     result = num1 * num2;
     System.Console.WriteLine("Natija: " + result);
+    System.Console.WriteLine("Davom etasizmi!");
+        DavomEt = Console.ReadLine();
+        if(DavomEt == "yes")
+        {
+            goto Salom;
+        }
+        else if(DavomEt == "NO")
+        {
+            System.Console.WriteLine("Goodbay");
+            break;
+        }
     break;
 
 }
