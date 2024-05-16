c60813fb9c38c4cadf3f38e5478cbf0f91b969ce
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-05-17 01:25:54 +0500

    Code Refaktor Calculator

---
+++ Classes/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator.Classes
+{
+    internal class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public string Operation { get; set; }
+
+        public void GetInputs()
+        {
+            Console.WriteLine("Type 1st number: ");
+            FirstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter operation(+,-,*,/,%): ");
+            Operation = Console.ReadLine();
+            Console.WriteLine("Type 2nd number: ");
+            SecondNumber = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,4 +2,6 @@
 using SimpleCalculator.Classes;
 
 Security security = new Security();
+Calculator calculator = new Calculator();
 security.CheckPassword();
+calculator.GetInputs();
