c5da6ed64628a6e6c507898440c87c41b2ae01d5
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-05-17 00:47:53 +0500

    Code Refaoktor

---
+++ Classes/Security.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator.Classes
+{
+    internal class Security
+    {
+        public void CheckPassword()
+        {
+            string password = "";
+
+            do
+            {
+                Console.WriteLine("Enter password: ");
+                password = Console.ReadLine();
+            } while (password != "pa$$w0rd");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,53 +1,5 @@
-﻿// ---------------------------------------------------------------
-// Copyright (c) Coalition of the Good-Hearted Engineers
-// FREE TO USE TO CONNECT THE WORLD
-// ---------------------------------------------------------------
-double num1, num2, result;
-char operation;
-string DavomEt;
-Salom:
-System.Console.WriteLine("Kalkulyatorga xush kelibsiz!");
-System.Console.Write("Birinchi sonni kiriting: ");
-num1 = Convert.ToDouble(Console.ReadLine());
-System.Console.Write("Operator kiriting: ");
-operation = Convert.ToChar(Console.ReadLine());
-System.Console.Write("Ikkinchi sonni kiriting: ");
-num2 = Convert.ToDouble(Console.ReadLine());
+﻿
+using SimpleCalculator.Classes;
 
-switch (operation)
-{
-    case '+':
-        result = num1 + num2;
-        System.Console.WriteLine("Natija: " + result);
-        break;
-    case '-':
-        result = num1 - num2;
-        System.Console.WriteLine("Natija: " + result);
-        break;
-    case '/':
-        if(num2 != 0)
-        {
-            result = num1 / num2;
-            System.Console.WriteLine("Natija: " + result);
-        }
-        else
-        {
-            System.Console.WriteLine("Nolga bo'lish mumkin emas!");
-        }
-        break;
-    case '*':
-        result = num1 * num2;
-        System.Console.WriteLine("Natija: " + result);
-        break;
-}
-System.Console.WriteLine("Davom etasizmi!");
-DavomEt = Console.ReadLine();
-if(DavomEt == "yes")
-{
-    goto Salom;
-}
-else if(DavomEt == "NO")
-{
-    System.Console.WriteLine("Goodbay");
-    
-}
+Security security = new Security();
+security.CheckPassword();
